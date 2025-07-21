c396793c97d20daf7e4cdd67c253ad915e18ffc2
Carlos Henrique <103267914+Carlos-H1307@users.noreply.github.com>
2025-07-21 04:24:44 -0300

    css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Menu from './pages/Menu';
-import FortuneRoulette from './pages/FortuneRoulette';
 import CoinFlip from './pages/CoinFlip';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { MessageProvider } from './contexts/MessageContext';
 
 function App() {
   return (
-    <MessageProvider>
       <Router>
         <Routes>
           <Route path="" element={<CoinFlip />} />
         </Routes>
       </Router>
-    </MessageProvider>
   );
 }
 
